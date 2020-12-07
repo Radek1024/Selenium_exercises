046aaf418746dd682506983f5698273d835f9d88
Dev Mąż <yakuzatanaka@gmail.com>
2020-12-08 00:24:40 +0100

    created exercise 1 & 2

---
+++ src/main/java/testCases/Exercise1.java
@@ -0,0 +1,28 @@
+package testCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.gecko.driver","D:\\geckodriver-v0.28.0-win64\\geckodriver.exe");
+
+        FirefoxOptions capabilities = new FirefoxOptions();
+        capabilities.setCapability("marionette",true);
+        WebDriver driver = new FirefoxDriver(capabilities);
+
+        driver.navigate().to("https://antycaptcha.amberteam.pl/exercises/exercise1?seed=4ba0ec83-95c5-4952-8f41-69e4e749413d");
+
+        WebElement clickButton1 = driver.findElement(By.id("btnButton1"));
+        WebElement clickButton2 = driver.findElement(By.id("btnButton2"));
+
+        clickButton1.click();
+        clickButton2.click();
+        clickButton1.click();
+
+    }
+}
---
+++ src/main/java/testCases/Exercise2.java
@@ -0,0 +1,29 @@
+package testCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.gecko.driver","D:\\geckodriver-v0.28.0-win64\\geckodriver.exe");
+
+        FirefoxOptions capabilities = new FirefoxOptions();
+        capabilities.setCapability("marionette",true);
+        WebDriver driver = new FirefoxDriver(capabilities);
+
+        driver.navigate().to("https://antycaptcha.amberteam.pl/exercises/exercise2?seed=4ba0ec83-95c5-4952-8f41-69e4e749413d");
+        driver.findElement(By.id("t14")).clear();
+
+        WebElement clearT14 = driver.findElement(By.id("t14"));
+        WebElement editT14 = driver.findElement(By.id("t14"));
+        WebElement clickButton = driver.findElement(By.id("btnButton1"));
+
+        clearT14.clear();
+        editT14.sendKeys("Type theory air.");
+        clickButton.click();
+    }
+}
